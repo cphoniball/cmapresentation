32adc88d31e330cf946ad3d5b233d13807fdd2a0
Chris Honiball <chris.p.honiball@gmail.com>
2014-07-28 16:33:29 -0500

    Added header, headroom.js

--- examples/client/js/main.js
+++ examples/client/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-	
+
+	var header = document.querySelector('.site-header'); 
+	var headroom = new Headroom(header, {
+		offset: 50, 
+		tolerance: 15
+	}); 
+	headroom.init(); 
+
 }); 
