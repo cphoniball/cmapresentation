ae18104ffc5a988af1c56f541ca562977925df3a
Chris Honiball <chris.p.honiball@gmail.com>
2014-07-28 17:27:48 -0500

    Footer

--- examples/client/js/main.js
+++ examples/client/js/main.js
@@ -2,9 +2,37 @@ $(document).ready(function() {
 
 	var header = document.querySelector('.site-header'); 
 	var headroom = new Headroom(header, {
-		offset: 50, 
+		offset: $(window).height() + 100, 
 		tolerance: 15
 	}); 
 	headroom.init(); 
+
+	function debounce(func, wait, immediate) {
+		var timeout;
+		return function() {
+			var context = this, args = arguments;
+			clearTimeout(timeout);
+			timeout = setTimeout(function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			}, wait);
+			if (immediate && !timeout) func.apply(context, args);
+		};
+	};
+
+	var changeNavItems = debounce(function() {
+		if ($(window).scrollTop() > $(window).height()) {
+			$('.site-navigation-list').addClass('hidden'); 
+			$('.page-navigation-list').removeClass('hidden'); 
+		}
+		else {
+			$('.site-navigation-list').removeClass('hidden'); 
+			$('.page-navigation-list').addClass('hidden'); 
+		}
+	}, 50); 
+
+	$(document).scroll(changeNavItems); 
+
+
 
 }); 
